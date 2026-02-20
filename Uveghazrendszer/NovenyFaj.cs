737c278c0b9086e3f4e653baa8491c0c8ce9ae21
Vitális Viktor <viktorka2030@gmail.com>
2026-02-20 09:27:42 +0100

    Novenyfaj osztaly kesz

--- Uveghazrendszer/NovenyFaj.cs
+++ Uveghazrendszer/NovenyFaj.cs
@@ -8,5 +8,31 @@ namespace Uveghazrendszer
 {
 	internal class NovenyFaj
 	{
+		string nev;
+		int nedvessegTartalom;
+		int egeszsegSzint;
+		int optimalisSuruseg;
+
+
+
+		public NovenyFaj(string nev, int optimalisSuruseg)
+		{
+			this.nev = nev;
+			this.optimalisSuruseg = optimalisSuruseg;
+			nedvessegTartalom = 100;
+			egeszsegSzint = 100;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int NedvessegTartalom { get => nedvessegTartalom; set => nedvessegTartalom = value; }
+		public int EgeszsegSzint { get => egeszsegSzint; set => egeszsegSzint = value; }
+		public int OptimalisSuruseg { get => optimalisSuruseg; set => optimalisSuruseg = value; }
+		public string Azonosito { get => Azonosito.Substring(0, 3); }
+
+		public override string ToString()
+		{
+			return $"{nev} nedevesség: {nedvessegTartalom}, egészség: {egeszsegSzint}, ideális egyedszám: {optimalisSuruseg}";
+		}
+
 	}
 }
