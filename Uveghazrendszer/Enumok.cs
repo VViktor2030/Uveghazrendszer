97a10cabc9a4cf8addfef56b596775c3d32b98de
Vitális Viktor <viktorka2030@gmail.com>
2026-02-20 09:16:38 +0100

    Kezelo osztaly kesz, Enumok osztaly letrehozva

---
+++ Uveghazrendszer/Enumok.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uveghazrendszer
+{
+		enum Szerepkor
+		{
+			KERTESZ,
+			TECHNIKUS,
+			ADMIN
+		}
+	internal class Enumok
+	{
+
+
+
+
+
+	}
+}
